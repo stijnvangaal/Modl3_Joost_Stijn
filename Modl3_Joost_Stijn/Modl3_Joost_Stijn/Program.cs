a594ef6c018127b9d4c701224b27dedb2aa4a7e1
Stijn van Gaal <stijn_50@msn.com>
2014-10-24 10:20:47 +0200

    setup

---
+++ Modl3_Joost_Stijn/Modl3_Joost_Stijn/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modl3_Joost_Stijn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
