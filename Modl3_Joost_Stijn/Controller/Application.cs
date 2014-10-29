eef8fc0ab60196db7dbb16149b876b089b870bbc
Stijn van Gaal <stijn_50@msn.com>
2014-10-29 15:46:26 +0100

    small startup

--- Modl3_Joost_Stijn/Controller/Application.cs
+++ Modl3_Joost_Stijn/Controller/Application.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Modl3_Joost_Stijn.View;
 
 namespace Modl3_Joost_Stijn.Controller
 {
     class Application
     {
+        private View.View myView { get; set; } 
     }
 }
