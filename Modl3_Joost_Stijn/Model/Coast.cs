e1083af17a6922e1110621e6d3456a90b8a0820e
joostvanrijsinge <jjrijsin@avans.nl>
2014-10-29 17:15:42 +0100

    dalda

---
+++ Modl3_Joost_Stijn/Model/Coast.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Modl3_Joost_Stijn.Model
+{
+    class Coast : Track
+    {
+        public Boat Boat { get; set; }
+
+
+    }
+}
